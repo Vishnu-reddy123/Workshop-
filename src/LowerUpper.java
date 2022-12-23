ec2d7b3b1878dc646689798b46fcb02844e6ab82
Vishnu-reddy123 <vishnu95532@gmail.com>
2022-12-23 10:41:40 +0530

    added logic to convert the word from upper case to lower case and lower case to upper case

---
+++ src/LowerUpper.java
@@ -0,0 +1,16 @@
+public class LowerUpper {
+    public static void main(String[] args) {
+    String str1 = "PrepInsTa";
+        StringBuffer newStr =new StringBuffer(str1);
+        for(int i=0;i<str1.length();i++){
+            if(Character.isLowerCase(str1.charAt(i))){
+                newStr.setCharAt(i,Character.toUpperCase(str1.charAt(i)));
+            }
+            else if(Character.isUpperCase(str1.charAt(i))){
+                newStr.setCharAt(i,Character.toLowerCase(str1.charAt(i)));
+
+            }
+            }
+        System.out.println("string after converting:" + newStr);
+        }
+}
